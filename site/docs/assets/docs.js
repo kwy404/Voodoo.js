882d6374e94bcada22c0cc9407f01f51264e8593
kwy404 <thekaway404@gmail.com>
2026-09-04 12:54:35 -0300

    0.10.1

--- site/docs/assets/docs.js
+++ site/docs/assets/docs.js
@@ -125,7 +125,7 @@
     })();
 
   var RAIZ = new URL('../', esteScript.src); // .../site/docs/
-  var RUNTIME = new URL('../voodoo.full.min.js?v=b25a38bc', RAIZ).href; // .../site/voodoo.full.min.js
+  var RUNTIME = new URL('../voodoo.full.min.js?v=175480dd', RAIZ).href; // .../site/voodoo.full.min.js
 
   // One level above the documentation is the site itself. Resolving it from
   // the script URL rather than from location means the header links work the
@@ -1047,7 +1047,7 @@
     return el('footer', {
       class: 'doc-rodape',
       html:
-        '<p>Voodoo.js 0.10.0 &#183; documentation.</p>' +
+        '<p>Voodoo.js 0.10.1 &#183; documentation.</p>' +
         '<p><a href="' +
         SITE +
         '">Home</a> &#183; ' +
